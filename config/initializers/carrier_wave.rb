eb60d669736290320fa9b11cece9aac3ba2bd9d5
Yazgylych <y.rahymov@gmail.com>
2018-02-26 09:26:31 -0500

    fog config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,13 +6,13 @@ CarrierWave.configure do |config|
     config.storage = :file
     config.enable_processing = false
   else
-    config.storage = :fog
-		config.fog_credentials = {
-		provider: 'AWS',
+    config.fog_credentials = {
+    provider: 'AWS',
     aws_access_key_id:     ENV['S3_ACCESS_KEY'],
     aws_secret_access_key: ENV['S3_SECRET_KEY'],
     region:            "us-east-1"
-  	}
+    }
+    config.storage = :fog
 		config.fog_directory= ENV['S3_BUCKET']
 	end
 end
