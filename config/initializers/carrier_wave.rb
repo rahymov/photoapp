a02aecd4d8249ae572912422bc8f35d2e5d0ff41
Yazgylych <y.rahymov@gmail.com>
2018-02-25 22:56:05 -0500

    fog

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
 	CarrierWave.configure do |config|
 		 config.fog_credentials = {
 		provider: 'AWS',
-    access_key_id:     ENV['aws_access_key'],
+    access_key_id:     ENV['aws_access_key_id'],
     secret_access_key: ENV['aws_secret_access_key'],
   	}
 
